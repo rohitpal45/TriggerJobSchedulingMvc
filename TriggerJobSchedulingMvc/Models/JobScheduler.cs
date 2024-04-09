2840b6b686cb9ab34a0f0b1aafb35f0ea2995cd2
rohitpal45 <rohitpal2580dotnet@gmail.com>
2024-04-09 09:43:34 +0530

    Add project files.

---
+++ TriggerJobSchedulingMvc/Models/JobScheduler.cs
@@ -0,0 +1,30 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TriggerJobSchedulingMvc.Models
+{
+    public class JobScheduler
+    {
+        public static void Start()
+        {
+            IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler();
+            scheduler.Start();
+
+            IJobDetail job = JobBuilder.Create<Jobclass>().Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+            .WithIdentity("trigger1", "group1")
+            .StartNow()
+            .WithSimpleSchedule(x => x
+            .WithIntervalInSeconds(10)
+            .RepeatForever())
+            .Build();
+
+            scheduler.ScheduleJob(job, trigger);
+        }
+    }
+}
---
+++ TriggerJobSchedulingMvc/Models/Jobclass.cs
@@ -0,0 +1,35 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace TriggerJobSchedulingMvc.Models
+{
+        public class Jobclass : IJob
+        {
+            public void Execute(IJobExecutionContext context)
+            {
+            var task = Task.Run(() =>
+            {
+                try
+                {
+                    // string path = HttpContext.Current.Server.MapPath("~/Content/Uplaod/Simple.txt");
+                    string path = System.Web.Hosting.HostingEnvironment.MapPath("~/Content/Uploads/Simple.txt");
+                    using (StreamWriter writer = new StreamWriter(path, true))
+                    {
+                        writer.WriteLine(string.Format("Quartz Scheduler Called on " + DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss tt")));
+                        writer.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                }
+            });
+        }
+    }
+}
